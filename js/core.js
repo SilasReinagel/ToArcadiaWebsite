b6b3b4567fa7f0d0eab2984a6d3fe2ea71091dc1
SilasReinagel <silas.reinagel@gmail.com>
2019-07-31 00:31:03 -0700

    Finished Email Subscribe feature

---
+++ js/core.js
@@ -0,0 +1,50 @@
+const byId = id => {
+  const e = document.getElementById(id);
+  if (!e)
+      console.log(`No element found with id: ${id}`);
+  return e;
+};
+
+const setVisibility = (id, shouldShow) => {
+  const displayStyle = shouldShow => shouldShow ? 'flex' : 'none';
+  const visibilityStyle = shouldShow => shouldShow ? 'visible' : 'hidden';
+  const e = byId(id);
+  e.style.display = displayStyle(shouldShow);
+  e.style.visibility = visibilityStyle(shouldShow);
+}
+
+const setValidationStatus = (e, isValid) => {
+  if (isValid) 
+    e.classList.remove("invalid-input");
+  else 
+    e.classList.add("invalid-input");
+}
+
+const baseInit = !window.onload ? () => {} : window.onload;
+const pipe = (f1, f2) => { f1(); f2(); };
+const addToOnLoad = (f) => window.onload = () => pipe(baseInit, f);
+
+const getJson = (url) => 
+    fetch(url)
+        .then(r => {
+            if (r.ok) 
+                return r.json();
+            else
+                throw Error(`Request rejected with status ${res.status}`);
+        });
+
+const postJson = (url, data) =>
+        fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data), 
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+        .then(r => {
+            if (r.ok) 
+                return r.json();
+            else
+                throw Error(`Request rejected with status ${res.status}`);
+        });
--- js/email-subcribe.js
+++ js/email-subcribe.js
@@ -1,86 +1,52 @@
-const byId = id => {
-    const e = document.getElementById(id);
-    if (!e)
-        console.log(`No element found with id: ${id}`);
-    return e;
-};
+const makeJsonIo = (slug) => ({    
+    put: (item) => 
+        getJson(`https://www.jsonstore.io/${slug}`)
+            .then(obj => obj.result || [])
+            .then(items => postJson(`https://www.jsonstore.io/${slug}`, items.concat([item])))
+            .then(item => item)
+});
 
+const makeLocalStorageIo = (key) => ({
+    put: (item) => new Promise(
+        (resolve, reject) => {
+            localStorage.setItem(key, item);
+            resolve(item);
+        })
+})
 
-const visibilityStyle = shouldShow => shouldShow ? 'flex' : 'none';
-const setVisibility = (id, shouldShow) => byId(id).style.display = visibilityStyle(shouldShow);
-
-const jsonIo = {
-    storeBaseUrl: 'https://www.jsonstore.io',
-    storeId: '6380571bd577406dfe41765b0c8d3b8cb6696338b988384cbda117ba589d5acc'
-};
-jsonIo.getStoreUrl = () => `${jsonIo.storeBaseUrl}/${jsonIo.storeId}`;
-jsonIo.withSavedEntries = (callback) => {
-    var req = new XMLHttpRequest();
-    var url = jsonIo.getStoreUrl();
-
-    req.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            var resp = JSON.parse(this.responseText);
-            callback(resp.result || [0]);
-        }
-    };
-    req.open("GET", url, true);
-    req.send();
-};
-jsonIo.appendEntry = (entries, entry) => {
-    entries.push(entry);
-
-    var req = new XMLHttpRequest();
-    var url = jsonIo.getStoreUrl();
-    req.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            if (this.status >= 200 && this.status < 300) {
-            } else {
-                console.log('Submission failed');
-            }
-        }
-    };
-    req.open("POST", url, true);
-    req.setRequestHeader("Content-Type", "application/json");
-    req.send(JSON.stringify(entries));
-};
-
-const localStorageEmailKey = 'email-subscription';
-const validateEmail = (email) => /\S+@\S+\.\S+/.test(email);
-const subscribe = (email) => {
-    jsonIo.withSavedEntries(entries => {
-        console.log({ email });
-        console.log({ entries });
-        jsonIo.appendEntry(entries, { subscribedAt: new Date().getTime(), email: email });
-        localStorage.setItem("email-subscription", { email, subscribed: true });
-        updateEmailVisibility();
-    });
-}
-
-const updateEmailVisibility = () => {
-    const hasEmailSubscribed = !!localStorage.getItem(localStorageEmailKey);
-    setVisibility('email-signup', !hasEmailSubscribed);
-    setVisibility('email-supporter', hasEmailSubscribed);
-}
-
-const init = () =>
+const initEmail = () =>
 {
+    const localStorageEmailKey = 'email-subscription';
+    const jsonIo = makeJsonIo('6380571bd577406dfe41765b0c8d3b8cb6696338b988384cbda117ba589d5acc');
+    const localStorageIo = makeLocalStorageIo(localStorageEmailKey);
+
+    const updateEmailVisibility = () => {
+        const hasEmailSubscribed = !!localStorage.getItem(localStorageEmailKey);        
+        setVisibility('email-signup', !hasEmailSubscribed);
+        setVisibility('email-supporter', hasEmailSubscribed);
+        setVisibility('email', true);
+    }
+
+    const validateEmail = (email) => /\S+@\S+\.\S+/.test(email);
+    const subscribe = (email) => 
+        jsonIo
+            .put({ subscribedAt: new Date().getTime(), email: email, subscribed: true })
+            .then(item => localStorageIo.put(item))
+            .then(_ => updateEmailVisibility())            
+            .catch(_ => console.log('Subscription failed'));
+
     updateEmailVisibility();
     byId('submit-email').onclick = function(e) {
         e.preventDefault();
 
-        const emailForm = byId('email-form');
-        const emailInput = emailForm.elements["email-entry"];
+        const emailInput = byId('email-form').elements["email-entry"];
         const email = emailInput.value;
-
-        var emailIsValid = validateEmail(email);
-        if (!emailIsValid) {
-            emailInput.classList.add("invalid-input");
-        } else {
-            emailInput.classList.remove("invalid-input");
+        const emailIsValid = validateEmail(email);
+        setValidationStatus(emailInput, emailIsValid);
+        if (emailIsValid) 
             subscribe(email);
-        }
     };
 };
 
-window.onload = init(); 
+
+addToOnLoad(initEmail);
